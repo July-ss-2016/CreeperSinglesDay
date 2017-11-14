45485afd6e251044eb975a549a44f762c693a032
JULY-SS-2016 <@17.HhJ~>
2017-11-14 16:12:21 +0800

    Initial commit

---
+++ src/vip/creeper/mcserverplugins/creepersinglesday/CreeperSinglesDay.java
@@ -0,0 +1,11 @@
+package vip.creeper.mcserverplugins.creepersinglesday;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CreeperSinglesDay extends JavaPlugin {
+    public void onEnable() {
+
+    }
+
+
+}
